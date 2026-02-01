1aa012b50298078bd52f13696d17e027f367b0f9
uhhbads <abadshoodee@gmail.com>
2026-02-01 15:59:48 +0800

    init

---
+++ src/test/java/com/practice/sprinttwo_librarymanagement/SprinttwoLibrarymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.sprinttwo_librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinttwoLibrarymanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
